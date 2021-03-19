498be3f0fb9a19eb4e0fcbb177a89beb6493bf76
Stepan Hojdar <hojdar.stepan@gmail.com>
2021-03-19 18:22:18 +0100

    Updates

---
+++ component---src-pages-404-js-e829f359f3413820e537.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return b}));t("q1tI");var o=t("Bl7J"),u=t("AeFk");function b(){return Object(u.b)(o.b,{headerText:"DemoWeb"},Object(u.b)("h1",null,"404 page not found."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e829f359f3413820e537.js.map
